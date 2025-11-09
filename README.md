b749021b7c3e40360c32f24b0f9904bb980cc1a8
vitorfreitasss <freitasvitorcastro40@gmail.com>
2025-11-09 12:48:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# TRABALHO-PAM
-TRABALHO PAM
+# Projeto001pam
--- js/main.js
+++ js/main.js
@@ -1,43 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('loginForm');
-  const email = document.getElementById('email');
-  const password = document.getElementById('password');
-  const togglePwd = document.getElementById('togglePwd');
-  const errorMsg = document.getElementById('error');
+function  mudacor(){
+    let  body = document.getElementById("bg");
+    let rand = Math.floor(Math.random() * 6);
 
-  // alternar visibilidade da senha
-  togglePwd.addEventListener('click', () => {
-    const isHidden = password.type === 'password';
-    password.type = isHidden ? 'text' : 'password';
-    togglePwd.textContent = isHidden ? '🙈' : '👁️';
-  });
-
-  // validar login simples
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const userEmail = email.value.trim();
-    const userPassword = password.value.trim();
-
-    if (!userEmail || !userPassword) {
-      showError('Preencha todos os campos.');
-      return;
+    switch(rand){
+        case 0:
+            body.style.backgroundColor = "#836FFF";
+        break;
+        case 1:
+            body.style.backgroundColor = "#6495ED";
+        break;
+        case 2:
+            body.style.backgroundColor = "#4682b4";
+        break;
+        case 3:
+            body.style.backgroundColor = "#008080";
+        break;
+        case 4:
+            body.style.backgroundColor = "#8b008b"; 
+        break;
     }
 
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(userEmail)) {
-      showError('E-mail inválido.');
-      return;
-    }
 
-    if (userEmail === 'user@teste.com' && userPassword === '123456') {
-      alert('Login bem-sucedido!');
-      form.reset();
-      errorMsg.textContent = '';
-    } else {
-      showError('E-mail ou senha incorretos.');
-    }
-  });
 
-  function showError(msg) {
-    errorMsg.textContent = msg;
-  }
-});
+};
